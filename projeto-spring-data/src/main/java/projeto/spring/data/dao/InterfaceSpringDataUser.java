c5d73484ca261b58792e9f3358b7346c90ae1cf1
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2021-08-17 20:14:25 -0300

    spring-data

--- projeto-spring-data/src/main/java/projeto/spring/data/dao/InterfaceSpringDataUser.java
+++ projeto-spring-data/src/main/java/projeto/spring/data/dao/InterfaceSpringDataUser.java
@@ -1,5 +1,6 @@
 package projeto.spring.data.dao;
 
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import projeto.spring.data.model.UsuarioSpringData;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 
 @Repository
@@ -17,12 +19,13 @@ public interface InterfaceSpringDataUser extends CrudRepository<UsuarioSpringDat
     @Query(value = "select p from UsuarioSpringData p where p.nome like %?1%")
     public List<UsuarioSpringData> buscaPorNome(String nome);
 
+    @Lock(LockModeType.READ) // Controle de Transação
     @Transactional(readOnly = true) // Apenas para leitura, esse código não pode fazer alteração no banaco
     @Query(value = "select p from UsuarioSpringData p where p.nome = :paramnome")
     public UsuarioSpringData buscaPorNomeParam(@Param("paramnome") String paramnome);
 
     @Modifying
-    @Transactional()
+    @Transactional
     @Query("delete from UsuarioSpringData u where u.nome = ?1")
     public void deletePorNome(String nome);
 
