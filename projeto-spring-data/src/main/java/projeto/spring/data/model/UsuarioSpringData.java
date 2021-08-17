c17f5e7e88ef4f9ad83e4d81b0384d7e07e6e325
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2021-08-17 18:08:45 -0300

    spring-data

---
+++ projeto-spring-data/src/main/java/projeto/spring/data/model/UsuarioSpringData.java
@@ -0,0 +1,29 @@
+package projeto.spring.data.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+
+@Entity
+public class UsuarioSpringData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String login;
+    private String senha;
+    private String nome;
+    private String email;
+    private int idade;
+
+
+}
--- projeto-spring-data/src/test/java/projeto/spring/data/AppSpringDataTest.java
+++ projeto-spring-data/src/test/java/projeto/spring/data/AppSpringDataTest.java
@@ -2,17 +2,71 @@ package projeto.spring.data;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import projeto.spring.data.dao.InterfaceSpringDataUser;
+import projeto.spring.data.model.UsuarioSpringData;
+
+import java.util.Optional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
 public class AppSpringDataTest {
 
+    @Autowired
+    private InterfaceSpringDataUser interfaceSpringDataUser;
+
+
     @Test
     public void testeInsert() {
-        System.out.println("Iniciou spring com success");
 
+        UsuarioSpringData usuarioSpringData = new UsuarioSpringData();
+        usuarioSpringData.setEmail("teste@gmail.com");
+        usuarioSpringData.setIdade(21);
+        usuarioSpringData.setLogin("teste 123");
+        usuarioSpringData.setSenha("123");
+        usuarioSpringData.setNome("teste");
+
+        interfaceSpringDataUser.save(usuarioSpringData);
+
+        System.out.println("Usuário cadastrado -> " + interfaceSpringDataUser.count());
+
+    }
+
+    @Test
+    public void testeConsulta() {
+        Optional<UsuarioSpringData> usuarioSpringData = interfaceSpringDataUser.findById(1L);
+        System.out.println(usuarioSpringData.get().getNome());
+        System.out.println(usuarioSpringData.get().getEmail());
+        System.out.println(usuarioSpringData.get().getIdade());
+        System.out.println(usuarioSpringData.get().getLogin());
+        System.out.println(usuarioSpringData.get().getSenha());
+        System.out.println(usuarioSpringData.get().getId());
+    }
+
+    @Test
+    public void testeConsultaTodos(){
+        Iterable<UsuarioSpringData> lista = interfaceSpringDataUser.findAll();
+
+        for (UsuarioSpringData usuarioSpringData : lista){
+            System.out.println(usuarioSpringData.getId());
+            System.out.println(usuarioSpringData.getNome());
+            System.out.println(usuarioSpringData.getEmail());
+            System.out.println(usuarioSpringData.getIdade());
+            System.out.println(usuarioSpringData.getLogin());
+            System.out.println(usuarioSpringData.getSenha());
+            System.out.println("---------------------------------------------------------");
+        }
+    }
+
+    @Test
+    public void testeUpdade(){
+        Optional<UsuarioSpringData> usuarioSpringData = interfaceSpringDataUser.findById(1L);
+
+        UsuarioSpringData data = usuarioSpringData.get();
+        data.setNome("Marcus Viniicus Update Spring dATA");
+        interfaceSpringDataUser.save(data);
     }
 
 }
